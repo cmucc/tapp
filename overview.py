047a3c8a44805509fbfacf349ab6d0228227986e
Sam Gruber <grubermensch@gmail.com>
2014-07-29 16:47:08 -0400

    add svg preamble matter

--- overview.py
+++ overview.py
@@ -64,6 +64,13 @@ def validate(data):
 def render(data):
   output = ''
 
+  # Preamble
+  output += '<?xml version="1.0"?>\n'
+  output += '<svg xmlns="http://www.w3.org/2000/svg" '
+  output += 'xmlns:xlink="http://www.w3.org/1999/xlink" '
+  output += 'viewBox="0 0 612 792">\n'
+
+  output += '</svg>'
   return output
 
 # Invoke main as top-level function
